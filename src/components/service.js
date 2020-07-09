c6e3f3a44d25eee8f4ddcb637724e1a900043116
Ben Inada <me@beninada.com>
2020-07-09 15:29:04 -0700

    finish first pass at services

--- src/components/service.js
+++ src/components/service.js
@@ -3,15 +3,17 @@ import React from "react"
 
 const Service = ({ title, list, description }) => {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className="col-span-1">
-        <h3 className="text-2xl">{title}</h3>
-        <ul>
-          {list.map((item, index) => <li key={index}>{item}</li>)}
-        </ul>
-      </div>
-      <div className="col-span-2">
-        {description}
+    <div className="py-6">
+      <h3 className="text-2xl mb-4">{title}</h3>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-1">
+          <ul>
+            {list.map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+        </div>
+        <div className="col-span-2">
+          {description}
+        </div>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,28 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Service from "../components/service"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Hr = styled.hr.attrs({
+  className: "border-gray-800"
+})``
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <section className="grid grid-cols-2 gap-6">
+    <section className="grid grid-cols-2 gap-6 container mx-auto py-12">
       <div className="text-3xl">Full service design and development studio. Your friendly neighborhood group of software experts who want to help.</div>
       <div style={{ maxWidth: `500px` }}>
         <Image />
       </div>
     </section>
-    <section>
-      <h2 className="text-3xl">Our Services</h2>
+    <section className="container-fluid py-12" style={{backgroundColor: "#EFEBE9"}}>
+      <div className="container mx-auto">
+      <h2 className="text-3xl mb-10">Our Services</h2>
+      <Hr />
       <Service
         title="Product Strategy"
         description="We validate your idea with future customers, de-risk your roadmap, and set the stage for the development of a product that delights users."
@@ -26,6 +33,52 @@ const IndexPage = () => (
           "Roadmap Planning",
         ]}
       />
+      <Hr />
+      <Service
+        title="UI/UX Design"
+        description="We bring your idea to light by creating renditions of your product that unify stakeholders and test concepts with users."
+        list={[
+          "Wireframes",
+          "High Fidelity Mockups",
+          "Interactive Prototypes",
+          "Graphic Design",
+        ]}
+      />
+      <Hr />
+      <Service
+        title="Software Engineering"
+        description="We build your product to last, using the latest tools & techniques to ensure your software supports growth at global scale."
+        list={[
+          "Full Stack Engineering",
+          "Platform & DevOps Engineering",
+          "Cloud Architecture",
+          "Desktop Applications",
+        ]}
+      />
+      <Hr />
+      <Service
+        title="Marketing &amp; Copywriting"
+        description="We bring your product to the world with a focused marketing strategy driven by data and cogent storytelling."
+        list={[
+          "SEO",
+          "Blogs",
+          "Social Media Marketing",
+          "Analytics",
+        ]}
+      />
+      <Hr />
+      <Service
+        title="Consulting"
+        description="We help your team move faster with more situational awareness, agility, and certainty."
+        list={[
+          "Performance & Security Audit",
+          "Agile Implementation",
+          "Design Thinking",
+          "Software Architecture",
+        ]}
+      />
+      <Hr />
+      </div>
     </section>
   </Layout>
 )

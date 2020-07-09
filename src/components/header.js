f388f73b304a01a4056cf5ef6bf1992c73816be6
Ben Inada <me@beninada.com>
2020-07-09 01:50:13 -0700

    convert header and layout to use tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -3,37 +3,15 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <Link
-        to="/"
-        style={{
-          textDecoration: `none`,
-        }}
-      >
-        Contact Us
+  <header className="container mx-auto py-5 mb-5 flex flex-row items-center">
+    <h1 className="text-4xl mr-auto">
+      <Link to="/">
+        {siteTitle}
       </Link>
-    </div>
+    </h1>
+    <Link to="/">
+      Contact Us
+    </Link>
   </header>
 )
 

448c8a8b348494b982f4d7cc793410e58e193351
Ben Inada <me@beninada.com>
2020-07-09 19:42:10 -0700

    add cabin font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultFontFamilyStack = ['Cabin', 'Helvetica', 'Arial', 'sans-serif'];
 module.exports = {
   purge: [],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': defaultFontFamilyStack,
+      'display': defaultFontFamilyStack,
+      'body': defaultFontFamilyStack,
+    }
   },
   variants: {},
   plugins: [],

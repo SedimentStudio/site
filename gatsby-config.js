8573b6445c1773124e8ba12000ad220bcfc808f4
Ben Inada <me@beninada.com>
2020-07-12 15:42:11 -0700

    deploy to s3

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "sedimentstudio.com",
+        protocol: "https",
+        hostname: "sedimenstudio.com",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

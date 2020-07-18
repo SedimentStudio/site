0d24209fe200039effa5c16a90c6106c563f0204
Ben Inada <me@beninada.com>
2020-07-18 02:02:41 -0700

    fix misplaced w-full

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,8 +25,8 @@ const Layout = ({ children }) => {
   return (
     <div class="flex flex-col min-h-screen">
       <Header siteTitle={data.site.siteMetadata.title} />
-      <main class="flex-grow w-full">{children}</main>
-      <footer className="container-fluid mx-auto bg-gray-600 text-white">
+      <main class="flex-grow">{children}</main>
+      <footer className="container-fluid w-full mx-auto bg-gray-600 text-white">
         <div className="flex container mx-auto px-8 py-8 sm:px-10">
           <a href="https://sedimentstudio.com" className="mr-auto">Sediment</a>
           <div>© {new Date().getFullYear()}</div>
